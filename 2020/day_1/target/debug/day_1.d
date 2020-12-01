220230ccac2ee74bdd18c30a5ba273607f3680e1
Phil Nadon <pnadon@securityscorecard.io>
2020-12-01 16:19:58 -0700

    added target to gitignore

--- 2020/day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/Users/pnadon/Desktop/aoc/2020/day_1/target/debug/day_1: /Users/pnadon/Desktop/aoc/2020/day_1/src/main.rs
--- 2020/day_1/target/debug/deps/day_1.d
+++
@@ -1,5 +0,0 @@
-/Users/pnadon/Desktop/aoc/2020/day_1/target/debug/deps/day_1: src/main.rs
-
-/Users/pnadon/Desktop/aoc/2020/day_1/target/debug/deps/day_1.d: src/main.rs
-
-src/main.rs:
