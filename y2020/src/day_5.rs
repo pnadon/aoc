836b1e7b1949301a2297e63ee1835dc12dd1eedb
Phil Nadon <pnadon@securityscorecard.io>
2020-12-05 10:45:58 -0700

    part 2 of day 5

--- y2020/src/day_5.rs
+++ y2020/src/day_5.rs
@@ -1,16 +1,28 @@
-pub fn part_1(input: &str) -> usize {
+pub fn part_1(input: &str) -> u16 {
     input.lines().map(|line| 
-        usize::from_str_radix(
-            &line.chars().map(|c| match c {
-                'F' | 'L' => '0',
-                'B' | 'R' => '1',
-                _ => panic!("Invalid char {:?}", c)
-            }).collect::<String>(),
-            2
-        ).unwrap()
+        seat_to_id(line).unwrap()
     ).max().unwrap()
 }
 
 pub fn part_2(input: &str) -> u16 {
-    0
+    let mut seats: [bool; 1024] = [false; 1024];
+    for line in input.lines() {
+        seats[seat_to_id(line).unwrap() as usize] = true;
+    }
+    seats.iter()
+        .enumerate()
+        .skip_while(|(_, i)| **i == false)
+        .find(|(_, seat)| !**seat)
+        .unwrap().0 as u16
+}
+
+fn seat_to_id(input: &str) -> Result<u16, std::num::ParseIntError> {
+    u16::from_str_radix(
+        &input.chars().map(|c| match c {
+            'F' | 'L' => '0',
+            'B' | 'R' => '1',
+            _ => panic!("Invalid char {:?}", c)
+        }).collect::<String>(),
+        2
+    )
 }
