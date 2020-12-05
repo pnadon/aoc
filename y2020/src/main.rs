2ee3aee52b6c45743c5748adcd644c79cc0b21b0
Philippe Nadon <phil@nadon.io>
2020-12-04 17:20:36 -0700

    finished day 4

--- y2020/src/main.rs
+++ y2020/src/main.rs
@@ -2,6 +2,7 @@ use std::env::args;
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 fn main() {
     let day = args().nth(1)
         .expect("Must pass at least 1 param")
@@ -17,12 +18,17 @@ fn main() {
             let input = args().nth(2).expect("Must pass in input string");
             println!("{:?}", day_2::part_1(&input));
             println!("{:?}", day_2::part_2(&input));
-        }
+        },
         3 => {
             let input = args().nth(2).unwrap();
             println!("{:?}", day_3::part_1(&input));
             println!("{:?}", day_3::part_2(&input));
-        }
+        },
+        4 => {
+            let input = args().nth(2).unwrap();
+            println!("{:?}", day_4::part_1(&input));
+            println!("{:?}", day_4::part_2(&input));
+        },
         _ => {
             println!("Invalid input");
         }
